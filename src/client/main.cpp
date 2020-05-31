f5d969204b982629a7eb57975d953e3741c755ae
makerslet <oshuev.valery@yandex.ru>
2020-05-31 14:40:44 +0300

    Сделал протокол статической библиотекой и слинковал с приложениями

---
+++ src/client/main.cpp
@@ -0,0 +1,7 @@
+#include "messages.pb.h"
+
+int main()
+{
+    protocol::text_message_request msg;
+	return 0;
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,7 @@
+#include "root.pb.h"
+
+int main()
+{
+    protocol::root_message msg;
+	return 0;
+}
